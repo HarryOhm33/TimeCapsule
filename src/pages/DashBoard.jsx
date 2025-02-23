305f43142ec2752979e6723610e643e1cec54db3
Hari Om <hari333333om@gmail.com>
2025-02-24 00:36:52 +0530

    fixed bugs

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -41,11 +41,7 @@ const Dashboard = () => {
           console.warn("No token found. User might not be logged in.");
         }
 
-        const response = await axios.get(`${API_BASE_URL}/api/mycapsule/`, {
-          headers: {
-            Authorization: `Bearer ${token}`, // ✅ Send token in headers
-          },
-        });
+        const response = await axios.get(`${API_BASE_URL}/api/mycapsule/`);
 
         setCapsules(Array.isArray(response.data) ? response.data : []);
       } catch (error) {
